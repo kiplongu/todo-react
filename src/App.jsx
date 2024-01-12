ff73e1d6bc5072c89d97c5136608bb7724de7a90
kiplongu <rodgersbiwott2016@gmail.com>
2024-01-12 10:51:39 +0300

    updated code with errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,72 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+
 function usePrevious(value) {
-  const ref = useRef(null);
+  const ref = useRef();
   useEffect(() => {
     ref.current = value;
+    
   });
   return ref.current;
 }
 
 const FILTER_MAP = {
   All: () => true,
-  Active: (task) => !task.completed,
-  Completed: (task) => task.completed,
+  Active: task => !task.completed,
+  Completed: task => task.completed
 };
 
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
-  const [filter, setFilter] = useState("All");
+  const [filter, setFilter] = useState('All');
 
   function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map((task) => {
+    if (Number(id) === NaN) {
+      return;
+    }
+    const updatedTasks = tasks.map(task => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
         // use object spread to make a new obkect
         // whose `completed` prop has been inverted
-        return { ...task, completed: !task.completed };
+        return { ...task, completed: !task.completed }
       }
       return task;
     });
     setTasks(updatedTasks);
   }
 
+
   function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
+    const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks);
   }
 
+
   function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
+    const editedTaskList = tasks.map(task => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
-        // Copy the task and update its name
-        return { ...task, name: newName };
+        if (task.editable == true) {
+          const newTask = { ...task, name: newName };
+          return newTask;
+        }
       }
-      // Return the original task if it's not the edited task
       return task;
     });
     setTasks(editedTaskList);
   }
 
+
   const taskList = tasks
-    ?.filter(FILTER_MAP[filter])
-    .map((task) => (
+    .filter(FILTER_MAP[filter])
+    .map(task => (
       <Todo
         id={task.id}
         name={task.name}
@@ -69,7 +78,7 @@ function App(props) {
       />
     ));
 
-  const filterList = FILTER_NAMES.map((name) => (
+  const filterList = FILTER_NAMES.map(name => (
     <FilterButton
       key={name}
       name={name}
@@ -83,33 +92,53 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   const listHeadingRef = useRef(null);
   const prevTaskLength = usePrevious(tasks.length);
+  const alternativeUrl = "http://todo.org";
+
+  // function getIntegrity() {
+  //   "sha384-oqVuAfXRKap7fdgcCY5uykM6+R9GqQ8K/uxy9rx7HNQlGYl1kPzQho1wx4JwY8wC";
+  // }
 
   useEffect(() => {
-    if (tasks.length < prevTaskLength) {
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (isDarkMode) {
+      const script = document.createElement("script"); // Sensitive
+      script.src = "https://cdnexample.com/dark-mode-styles.js";
+      // script.integrity = getIntegrity();
+      document.head.appendChild(script);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
       listHeadingRef.current.focus();
     }
-  }, [tasks.length, prevTaskLength]);
+    return;
+  }, [tasks.length, , prevTaskLength]);
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">{filterList}</div>
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
       <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
         {headingText}
       </h2>
       <ul
-        aria-labelledby="list-heading"
-        className="todo-list stack-large stack-exception"
         role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
       >
         {taskList}
       </ul>
+      <a href={alternativeUrl}>Use another TODO tracker</a>
     </div>
   );
 }
